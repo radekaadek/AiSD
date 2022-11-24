a187209ae4d3f742b62070ff592f5cb53d198bc6
radekaadek <radekaadek@gmail.com>
2022-11-24 17:00:19 +0100

    readability

--- calc/325683_calc_1.cpp
+++ calc/325683_calc_1.cpp
@@ -7,31 +7,35 @@
 #include <iostream>
 #include <stack>
 
-
 int main() {
 	// Deklaracja zmiennych
 	std::stack<int> value_stack;
 	std::stack<char> operator_stack;
 	char token, operator_token;
 	int value_1, value_2; // Wartosci zdejmowane ze stosow
-	uint64_t bracket_count = 0;
+
 	// Wczytywanie danych
 	while(std::cin.get(token)) {
 		switch (token) {
 		case '(':
-			bracket_count++;
 		case ' ':
 			break;
+		// Operatory laduje na stos
+		case '+':
+		case '*':
+		case '/':
+		case '-':
+			operator_stack.push(token);
+			break;
 		case ')':
-			bracket_count--;
+		    // Zdejmuje operatory ze stosow i wykonuje dzialanie
+			// Wynik wraca na stos
 			value_2 = value_stack.top();
 			value_stack.pop();
 			value_1 = value_stack.top();
 			value_stack.pop();
 			operator_token = operator_stack.top();
 			operator_stack.pop();
-			// Wykonujemy operacje na zdjetych wartosciach
-			// Wynik wraca na stos
 			switch (operator_token) {
 			case '+':
 				value_stack.push(value_1 + value_2);
@@ -43,6 +47,7 @@ int main() {
 				value_stack.push(value_1 * value_2);
 				break;
 			case '/':
+				// Sprawdzanie dzielenia przez 0
 				if (!value_2) {
 					std::cout << "NaN";
 					exit(1);
@@ -51,21 +56,12 @@ int main() {
 				break;
 			}
 			break;
-		// Operatory i cyfry ladujemy na stos
-		case '+':
-		case '*':
-		case '/':
-			operator_stack.push(token);
-			break;
-		case '-':
-			operator_stack.push('-');
-			break;
+		// Cyfry ladujemy na stos
 		default:
 			value_stack.push(token - '0');
 			break;
 		}
 	}
-	// Wypisujemy wynik
 	std::cout << value_stack.top() << std::endl;
 	return 0;
 }
